2ede362a1632248a3e19e9545369337358ec3305
Joseph March <josephmarch70@gmail.com>
2026-03-01 16:53:56 +0000

    added function to load game and detect keypress

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,3 +1,10 @@
+use chip8_core::*;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len != 2 {
+        println!("Usage: cargo run path/to/game");
+        return;
+    }
 }
